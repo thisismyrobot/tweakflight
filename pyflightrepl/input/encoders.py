090138ac43dafd07149a87cda04266a26b5ba072
Robert Wallhead <rwallhead@gmail.com>
2017-09-13 11:30:10 +1000

    Working Encoders!!!

--- pyflightrepl/input/encoders.py
+++ pyflightrepl/input/encoders.py
@@ -1,7 +1,20 @@
-"""Input from a pair of rotary encoders."""
+"""Input from a pair of rotary encoders.
+
+Maps:
+    * Left encoder turn = prev and next setting (CW = next setting).
+    * Right encoder turn = prev and next value (CW = positive increase).
+    * Both buttons pushed = Save.
+"""
 import collections
+import queue
 
 import RPi.GPIO as gpio
+
+import pyflightrepl.commands as commands
+
+
+__SETUP = False
+__CMD = queue.Queue()
 
 
 # GPIO assignments for encoders, using Board pins. A or B is the 'channel' on
@@ -10,8 +23,12 @@ LEFT_A = 38
 LEFT_B = 40
 LEFT_PUSH = 36
 
+RIGHT_A = 35
+RIGHT_B = 37
+RIGHT_PUSH = 33
 
-def setup():
+
+def setup_pins():
     """Set up the pins."""
 
     # Use Board Pin numbers
@@ -21,27 +38,29 @@ def setup():
     gpio.setup(LEFT_A, gpio.IN, pull_up_down=gpio.PUD_UP)
     gpio.setup(LEFT_B, gpio.IN, pull_up_down=gpio.PUD_UP)
     gpio.setup(LEFT_PUSH, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(RIGHT_A, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(RIGHT_B, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(RIGHT_PUSH, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 
-def track_encoder(pin_A, pin_B, pin_push):
+def track_encoder(pin_A, pin_B, cw_command, ccw_command):
     """Track an encoder."""
-
-    # Encoder
     data_buffer = collections.OrderedDict()
-    cw = ((0, 1), (0, 0), (1, 0), (1, 1))
-    ccw = ((1, 0), (0, 0), (0, 1), (1, 1))
+    cw_pattern = ((0, 1), (0, 0), (1, 0), (1, 1))
+    ccw_oattern = ((1, 0), (0, 0), (0, 1), (1, 1))
 
     def step(_):
+        """Handle a change to either command pin on the encoder."""
         current_a = gpio.input(pin_A)
         current_b = gpio.input(pin_B)
 
         # Dict keys can't duplicate, stripping noise. Order tells direction.
         data_buffer[(current_a, current_b)] = None
 
-        if tuple(data_buffer.keys()) == cw:
-            print('CW!')
-        elif tuple(data_buffer.keys()) == ccw:
-            print('CCW!')
+        if tuple(data_buffer.keys()) == cw_pattern:
+            __CMD.put(cw_command)
+        elif tuple(data_buffer.keys()) == ccw_oattern:
+            __CMD.put(ccw_command)
 
         if current_a == current_b == 1 or len(data_buffer) == 4:
             data_buffer.clear()
@@ -49,15 +68,27 @@ def track_encoder(pin_A, pin_B, pin_push):
     gpio.add_event_detect(pin_A, gpio.BOTH, callback=step)
     gpio.add_event_detect(pin_B, gpio.BOTH, callback=step)
 
-    # Push button
+
+def track_button_pair(pin_left_push, pin_right_push, push_command):
+    """Track both pins being pushed."""
     def push(_):
-        print('push!')
+        """Handle either button being pushed."""
+        if gpio.input(pin_left_push) == gpio.input(pin_right_push) == 1:
+            __CMD.put(push_command)
 
-    gpio.add_event_detect(LEFT_PUSH, gpio.FALLING, callback=push, bouncetime=300)
+    gpio.add_event_detect(pin_left_push, gpio.BOTH, callback=push, bouncetime=300)
+    gpio.add_event_detect(pin_right_push, gpio.BOTH, callback=push, bouncetime=300)
 
-    input()
 
+def read_blocking():
+    """The function called by the REPL."""
+    global __SETUP
+    if not __SETUP:
+        setup_pins()
+        track_encoder(LEFT_A, LEFT_B, commands.NEXT_SETTING, commands.PREV_SETTING)
+        track_encoder(RIGHT_A, RIGHT_B, commands.INC_VALUE, commands.DEC_VALUE)
+        track_button_pair(LEFT_PUSH, RIGHT_PUSH, commands.SAVE)
 
-if __name__ == '__main__':
-    setup()
-    track_encoder(LEFT_A, LEFT_B, LEFT_PUSH)
+        __SETUP = True
+
+    return __CMD.get()
